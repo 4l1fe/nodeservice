d0c09fe4d364b317bbdaaac4109d0edd0ed57180
4l1fe <4l1fe@users.noreply.github.com>
2014-08-20 12:01:39 +0400

    поправлен node_requirements.sh

--- node_requirements.sh
+++ node_requirements.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sudo npm install -g js-yaml formidable jade ws coffe
-sudo npm install git+https://github.com/dotcloud/zerorpc-node.git
+sudo npm install -g js-yaml formidable jade ws coffee
+sudo npm install -g git+https://github.com/dotcloud/zerorpc-node.git
