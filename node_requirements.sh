48230470d67e9552437e0ec5f3fc905370e7c4ec
Ivan <ikrivosheev@aaysm.com>
2014-08-22 19:00:16 +0400

    Добавлен пакет

--- node_requirements.sh
+++ node_requirements.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sudo npm install -g js-yaml formidable jade ws coffee MD5
+sudo npm install -g js-yaml formidable jade ws coffee MD5 ua-parser
 sudo npm install -g git+https://github.com/dotcloud/zerorpc-node.git
